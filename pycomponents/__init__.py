81701e3fb5cd8251275ba4413e3fe95137fdf262
Bodor Dávid Gábor <david.gabor.bodor@gmail.com>
2015-06-28 18:54:00 +0200

    Add World class to wrap around ECS system

---
+++ pycomponents/__init__.py
@@ -0,0 +1,96 @@
+class Entity(object):
+    next_id = 0
+
+    def __init__(self, *components):
+        self.id = Entity.next_id
+        Entity.next_id += 1
+
+        self.components = set()
+        for component in components:
+            self.add(component)
+
+    def add(self, component):
+        if type(component) == type:
+            component = component()
+        setattr(self, component.__class__.__name__, component)
+        self.components.add(component.__class__)
+
+    def remove(self, component_class):
+        self.components.remove(component_class)
+
+    def has(self, *component_classes):
+        return set(component_classes).issubset(self.components)
+
+
+def Component(name, **attributes):
+    """Returns a new component"""
+    attributes['__init__'] = Component__init__
+    return type(name, (), dict(**attributes))
+
+
+def Component__init__(self, **attributes):
+        if not set(attributes).issubset(set(self.__class__.__dict__)):
+            raise AttributeError("No such attribute in class")
+        self.__dict__.update(attributes)
+
+
+def System(components, **attributes):
+    """A decorator to create a system definition."""
+    def system_inner(fn):
+        fn.components = tuple(components)
+        fn.__dict__.update(attributes)
+
+        def fn__init__(**inner_attributes):
+            if not set(inner_attributes).issubset(set(attributes)):
+                raise AttributeError("No such attribute on function")
+            fn.__dict__.update(inner_attributes)
+            fn__init__.__dict__.update(fn.__dict__)
+            return fn
+        return fn__init__
+    return system_inner
+
+
+class World(object):
+    def __init__(self):
+        self._entities = []
+        self._systems = []
+    def add(self, *objs):
+        for obj in objs:
+            if isinstance(obj, Entity):
+                self.add_entity(obj)
+            elif callable(obj):
+                self.add_system(obj)
+
+    def add_entity(self, entity):
+        self._entities.append(entity)
+
+    def add_system(self, system):
+        self._systems.append(system)
+
+    def entities(self, has=None):
+        if not has:
+            return list(self._entities)
+        else:
+            components = has
+            return list(filter(lambda e: e.has(*components), self._entities))
+
+    def systems(self):
+        return list(self._systems)
+
+    def remove(self, obj):
+        if isinstance(obj, Entity):
+            self.remove_entity(obj)
+        elif callable(obj):
+            self.remove_system(obj)
+
+    def remove_entity(self, entity):
+        self._entities.remove(entity)
+
+    def remove_system(self, system):
+        self._systems.remove(system)
+
+    def update(self, dt):
+        self.dt = dt
+        for system in self.systems():
+            for entity in self.entities(has=system.components):
+                system(entity, self)
--- pycomponents/test_entity.py
+++ pycomponents/test_entity.py
@@ -1,16 +1,30 @@
 import unittest
 import typesafety
 
-from .entity import Entity, Component, system
+from . import Entity, Component, System, World
 
 Position = Component('position', x=0, y=0)
 Velocity = Component('velocity', x=0, y=0)
+
+@System([Position, Velocity], dampening=0.9)
+def physics(entity, world):
+    entity.position.x += entity.velocity.x * world.dt
+    entity.position.y += entity.velocity.y * world.dt
+
+    factor = max(0, (1 - physics.dampening))
+    entity.velocity.x *= factor
+    entity.velocity.y *= factor
 
 
 class BaseTestCase(unittest.TestCase):
     def assertHasAttr(self, obj, attr):
         if not hasattr(obj, attr):
             msg = "{} has no attribute '{}'".format(obj, attr)
+            raise AssertionError(msg)
+
+    def assertLen(self, seq, length):
+        if not len(seq) == length:
+            msg = "{} should have length of '{}'".format(seq, length)
             raise AssertionError(msg)
 
 
@@ -49,6 +63,13 @@ class TestEntityComponent(BaseTestCase):
         entity.remove(Position)
         self.assertFalse(entity.has(Position))
 
+    def test_has_tests_if_entity_has_all_components(self):
+        entity = Entity(Position, Velocity)
+        self.assertTrue(entity.has(Position, Velocity))
+
+        entity.remove(Velocity)
+        self.assertFalse(entity.has(Position, Velocity))
+
 
 class TestComponent(BaseTestCase):
     def test_can_only_be_instantiated_with_predefined_attributes(self):
@@ -62,21 +83,72 @@ class TestComponent(BaseTestCase):
         self.assertTrue(Interval(low=10, high=15).diff, 5)
 
 
-@system(Position, Velocity, dampening=0.9)
-def physics(entity, world):
-    entity.position.x += entity.velocity.x * world.dt
-    entity.position.y += entity.velocity.y * world.dt
-
-    entity.velocity.x *= physics.dampening
-    entity.velocity.y *= physics.dampening
-
-
 class TestSystem(BaseTestCase):
     def test_system_can_be_created(self):
         p = physics()
         self.assertEquals(p.components, (Position, Velocity))
         self.assertEquals(p.dampening, 0.9)
 
-    def test_system_can_have_variables(self):
+    def test_system_can_have_tweakable_variables(self):
         p = physics(dampening=0.8)
         self.assertEquals(p.dampening, 0.8)
+
+
+class TestWorld(BaseTestCase):
+    def test_can_add_entities_to_world(self):
+        world = World()
+        self.assertLen(world.entities(), 0)
+
+        world.add(Entity())
+        self.assertLen(world.entities(), 1)
+
+    def test_can_get_entities_by_component(self):
+        world = World()
+        world.add(
+            Entity(Position, Velocity), Entity(),
+            Entity(Position), Entity(Velocity))
+
+        selection = world.entities(has=[Position])
+        self.assertLen(selection, 2)
+        for entity in selection:
+            self.assertTrue(entity.has(Position))
+
+    def test_can_remove_entities(self):
+        world = World()
+        entity1 = Entity()
+        entity2 = Entity(Velocity)
+        world.add(entity1, entity2)
+        self.assertIn(entity1, world.entities())
+        self.assertIn(entity2, world.entities())
+
+        world.remove(entity1)
+        self.assertNotIn(entity1, world.entities())
+        self.assertIn(entity2, world.entities())
+
+
+    def test_can_add_systems(self):
+        world = World()
+        world.add(physics())
+        self.assertLen(world.systems(), 1)
+
+    def test_can_remove_systems(self):
+        world = World()
+        p = physics()
+        world.add(p)
+        self.assertLen(world.systems(), 1)
+        world.remove(p)
+        self.assertLen(world.systems(), 0)
+
+    def test_update_calls_system_on_relevant_elements(self):
+        world = World()
+        world.add(physics(dampening=0.0))
+
+        entity = Entity(Position(x=0, y=0), Velocity(x=1, y=1))
+        world.add(entity)
+
+        world.update(dt = 1)
+        self.assertEquals(entity.position.x, 1)
+
+        world.update(dt = 1)
+        self.assertEquals(entity.position.x, 2)
+
