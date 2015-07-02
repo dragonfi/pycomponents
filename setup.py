b977cf85a16d322cecb2bde051ad3026fa8e037e
Bodor Dávid Gábor <david.gabor.bodor@gmail.com>
2015-07-02 20:23:29 +0200

    Make package installable via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="pycomponents",
+    version='0.1',
+    description="A simple component entity system for python",
+    author="David Gabor Bodor",
+    url="http://github.com/dragonfi/pycomponents",
+    packages=['pycomponents'],
+    license="MIT",
+)
